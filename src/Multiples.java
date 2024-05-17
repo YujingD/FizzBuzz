dbfea944e7ea4dffbedceeacad587dec60c59775
Yujing Duan <yujing.duan@mail.utoronto.ca>
2024-05-17 00:30:25 -0400

    counter numbers which is either the multiples of 3 or 5

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Multiples {
+public class Multiples {
+
+    public static void main(String[] args) {
+
+        //how many multiples of 3 or 5 there are below 1000
+        int counter = 0;
+        for (int i = 1; i < 1000; i++) {
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            if (divisibleBy3 || divisibleBy5) {
+                counter++;
+            }
+
+        }
+        System.out.println(counter);
+    }
 }
